fd9bfb96eed23c2df6b4eb301e6951576289e21a
ABHILASH S S <abhilashss@ABHILASHs-MacBook-Pro.local>
2024-03-08 13:26:27 +0530

    test: add e2e for search endpoint

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,15 +1,25 @@
 import * as request from 'supertest';
 import { Test } from '@nestjs/testing';
-import { AppModule } from './../src/app.module';
 import { INestApplication } from '@nestjs/common';
+import { config } from 'dotenv';
+import { AppModule } from './../src/app.module';
+import { SearchService } from './../src/search/search.service';
 
-describe('AppController (e2e)', () => {
+config({ path: '.env.test' });
+
+describe('AppModule (e2e)', () => {
   let app: INestApplication;
+  const searchServiceMock = {
+    getFlights: jest.fn(),
+  };
 
   beforeAll(async () => {
     const moduleFixture = await Test.createTestingModule({
       imports: [AppModule],
-    }).compile();
+    })
+      .overrideProvider(SearchService)
+      .useValue(searchServiceMock)
+      .compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
@@ -24,5 +34,36 @@ describe('AppController (e2e)', () => {
       .get('/')
       .expect(200)
       .expect('Hello World!');
+  });
+
+  it('/search (GET)', async () => {
+    const mockFlightData = [
+      {
+        slices: {
+          origin_name: 'test',
+          destination_name: 'test',
+          departure_date_time_utc: 'test',
+          arrival_date_time_utc: 'test',
+          flight_number: 'test',
+          duration: 12,
+        },
+        price: 100,
+      },
+    ];
+
+    searchServiceMock.getFlights.mockResolvedValue(mockFlightData);
+    const response = await request(app.getHttpServer())
+      .get('/search')
+      .expect(200);
+
+    expect(response.body).toEqual(mockFlightData);
+  });
+
+  it('/search (GET) - should throw 500 if SearchService failed to get the data', async () => {
+    searchServiceMock.getFlights.mockRejectedValue(
+      new Error('Internal Server Error'),
+    );
+
+    await request(app.getHttpServer()).get('/search').expect(500);
   });
 });
